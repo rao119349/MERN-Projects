bd8f108bcbb2c09a1f5fef322dd4f2f393c4e4f2
Saurav Yadav <sauravyadav1193@gmail.com>
2024-10-28 23:30:42 +0530

    react refresher code

--- react-refresher-01-react-starting-project/src/components/NewGoal.js
+++ react-refresher-01-react-starting-project/src/components/NewGoal.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewGoal = props => {
-  let enteredText = '';
+  const [enteredText, setEnteredText] = useState('');
+
   const addGoalHandler = event => {
     event.preventDefault();
     const newGoal = { id: Math.random().toString(), text: enteredText };
 
+    setEnteredText('');
     props.onAddGoal(newGoal);
   };
 
   const textChangeHandler = event => {
-    enteredText = event.target.value;
+    setEnteredText(event.target.value);
   };
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" onChange={textChangeHandler} />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="Submit">Add Goal</button>
     </form>
   );
